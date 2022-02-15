9f52ae03468c4c243209ee26f87135c0aa34313f
omar saade <omarsaadeh11@gmail.com>
2022-02-15 16:47:50 +0200

    Random Property And Value From Object

--- hangm.js
+++ hangm.js
@@ -26,8 +26,49 @@ lettersArray.forEach(letter => {
 
     //append Span to The Letters Conatianer
     lettersContainer.appendChild(span);
-
-
-
-
 });
+
+//Object of Words + Categories
+const words = {
+    programming: ["php", "javascript", "go", "scala", "fortran", "r", "mysql", "python"],
+    movies: ["Prestige", "Inception", "Parasite", "Interstellar", "whiplash", "Memento", "Coco", "Up"],
+    people: ["Albert Einstein", "Hitchcock", "Alexander", "Cleopatra", "Mahatma Ghandi"],
+    countries: ["Syria", "Palestine", "Yemen", "Egypt", "Bahrain", "Qatar"],
+}
+
+//Get Random Property
+//bta3tik random keys;
+//    [programming , movies ,people , countries]
+let allkeys = Object.keys(words);
+
+//Random Number Depend On Keys Length
+
+//    0 To 3
+let randomPropNumber = Math.floor(Math.random() * allkeys.length);
+
+//Category
+//random category / btjeblak one Random category
+//programming , movies , people....
+let randomPropName = allkeys[randomPropNumber];
+
+//Category Words
+//btjeblak el array el 5assa bel category litna2et
+// (5) ['Albert Einstein', 'Hitchcock', 'Alexander', 'Cleopatra', 'Mahatma Ghandi']
+let randomPropValue = words[randomPropName];
+
+
+//Random Number Depend on Words
+// 0 to 4
+let randomValueNumber = Math.floor(Math.random() * randomPropValue.length);
+
+//The Chosen Words
+// btna2e el kelme men el array
+//Alexander
+let randomValueValue = randomPropValue[randomValueNumber];
+
+
+//set Category Info
+document.querySelector(".game-info .category span").innerHTML = randomPropName;
+
+
+
